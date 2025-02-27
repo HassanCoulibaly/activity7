025d8c39daba646fa241f53534ff4747d7db8a2f
Ritesh336 <rocky.x336@gmail.com>
2025-02-26 19:18:32 -0500

    commit Ritesh

--- lib/main.dart
+++ lib/main.dart
@@ -4,10 +4,18 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  bool _darkMode = false;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: _darkMode ? ThemeData.dark() : ThemeData.light(),
       home: FadingTextAnimation(),
     );
   }
@@ -20,6 +28,9 @@ class FadingTextAnimation extends StatefulWidget {
 
 class _FadingTextAnimationState extends State<FadingTextAnimation> {
   bool _isVisible = true;
+  bool _showFrame = false;
+  bool _darkMode = false;
+
   void toggleVisibility() {
     setState(() {
       _isVisible = !_isVisible;
@@ -31,20 +42,55 @@ class _FadingTextAnimationState extends State<FadingTextAnimation> {
     return Scaffold(
       appBar: AppBar(
         title: Text('Fading Text Animation'),
+        actions: <Widget>[
+          Switch(
+            value: _darkMode,
+            onChanged: (value) {
+              setState(() {
+                _darkMode = value;
+                (context as Element).markNeedsBuild();  // Forces the parent widget to rebuild with the new theme.
+              });
+            },
+          ),
+        ],
       ),
       body: Center(
-        child: AnimatedOpacity(
-          opacity: _isVisible ? 1.0 : 0.0,
-          duration: Duration(seconds: 1),
-          child: Text(
-            'Hello, Flutter!',
-            style: TextStyle(fontSize: 24),
+        child: Container(
+          decoration: _showFrame ? BoxDecoration(
+            border: Border.all(
+              color: Colors.blue,
+              width: 5,
+            ),
+          ) : null,
+          child: AnimatedOpacity(
+            opacity: _isVisible ? 1.0 : 0.0,
+            duration: Duration(seconds: 1),
+            child: Text(
+              'Hello, Flutter!',
+              style: TextStyle(fontSize: 24),
+            ),
           ),
         ),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: toggleVisibility,
         child: Icon(Icons.play_arrow),
+      ),
+      bottomNavigationBar: BottomAppBar(
+        child: Row(
+          children: [
+            Text('Show Frame'),
+            Switch(
+              value: _showFrame,
+              onChanged: (bool value) {
+                setState(() {
+                  _showFrame = value;
+                });
+              },
+            ),
+          ],
+          mainAxisAlignment: MainAxisAlignment.center,
+        ),
       ),
     );
   }
